142bdc232dee79ccfbd078ef2b788eb8b7b67a11
Matheus Nobre Casado <98357592+Nobre0309Y@users.noreply.github.com>
2024-01-24 23:09:17 -0300

    Adicionar arquivos de projeto.

---
+++ LetCodeProblems/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+
+        for (var fixoDaSoma = 0; fixoDaSoma < nums.Count(); fixoDaSoma++)
+        {
+            var aux = 0;
+            while (aux < nums.Count())
+            {
+                if (nums[fixoDaSoma] + nums[aux] == target && fixoDaSoma != aux)
+                {
+                    return new int[2] { fixoDaSoma, aux };
+                }
+                aux++;
+            }
+        }
+
+        return null;
+    }
+}
