379e7adca204d1d5ac665a42f4838ac13a8b0be1
Matheus Nobre Casado <98357592+Nobre0309Y@users.noreply.github.com>
2024-01-25 14:54:21 -0300

    estudando sobre ListNode

--- LetCodeProblems/AddTwoNumbers.cs
+++ LetCodeProblems/AddTwoNumbers.cs
@@ -19,44 +19,33 @@ namespace LetCodeProblems
 
     public class AddTwoNumbers
     {
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        public ListNode AddTwoNumbersCode(ListNode l1, ListNode l2)
         {
-            var List1 = new List<int>();
-            var List2 = new List<int>();
-             
-            while (l1.next != null)
-            {
-                List1.Add(l1.next.val);
-            }
-            while(l2.next != null)
-            {
-                List2.Add(l2.next.val);
-            }
+            ListNode result = new ListNode(l1.val + l2.val);
 
-            var sum1 = List1[0];
-            int aux = 0;
-            while (List1.Count() < aux)
-            {
-                sum1 +=  10 ^ aux * List1[aux] + sum1;
-                aux++;
-            }
 
-            int sum2 = List2[0];
-            aux = 0;
-            while (List2.Count() < aux)
-            {
-                sum2 += 10 ^ aux * List2[aux] + sum2;
-                aux++;
-            }
-            var sumTotal = sum1 + sum2; 
+            int sum = 0;
+            int sobe1 = 0;
 
-            var result = new ListNode();
-            while(sumTotal / 10 > 0)
+            while (l1.next != null || l2.next != null) 
             {
-                result.val = 
-            }
+                if (l1.next != null)
+                {
+                    sum += l1.next.val;
+                    l1= l1.next;
+                }
+                if (l2.next != null)
+                {
+                    sum += l2.next.val;
+                    l2 = l2.next;
+                }
+                sum += sobe1;
+                result = new ListNode(sum % 10);
+                sobe1 = (l1.val + l2.val) / 10; 
+                
 
-            return new ListNode();
+            }
+            return result;  
         }
 
     }
