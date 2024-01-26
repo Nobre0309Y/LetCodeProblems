3b0a323a71c7f24b195908bc128497b544abe5d8
Matheus Nobre Casado <98357592+Nobre0309Y@users.noreply.github.com>
2024-01-26 02:15:38 -0300

    Finalmente AddTwoNumbersCOde

--- LetCodeProblems/AddTwoNumbers.cs
+++ LetCodeProblems/AddTwoNumbers.cs
@@ -1,52 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LetCodeProblems
+﻿public class ListNode
 {
-    public class ListNode
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
     {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
+        this.val = val;
+        this.next = next;
     }
-
-    public class AddTwoNumbers
+}
+public class AddTwoNumbers
+{
+    public ListNode AddTwoNumbersCOde(ListNode l1, ListNode l2)
     {
-        public ListNode AddTwoNumbersCode(ListNode l1, ListNode l2)
+        ListNode result = new ListNode(0);
+        ListNode test = result;
+
+        int sum = 0;
+        int sobe1 = 0;
+
+
+        while (l1 != null || l2 != null)
         {
-            ListNode result = new ListNode(l1.val + l2.val);
 
-
-            int sum = 0;
-            int sobe1 = 0;
-
-            while (l1.next != null || l2.next != null) 
+            if (l1 != null)
             {
-                if (l1.next != null)
-                {
-                    sum += l1.next.val;
-                    l1= l1.next;
-                }
-                if (l2.next != null)
-                {
-                    sum += l2.next.val;
-                    l2 = l2.next;
-                }
-                sum += sobe1;
-                result = new ListNode(sum % 10);
-                sobe1 = (l1.val + l2.val) / 10; 
-                
-
+                sum += l1.val;
+                l1 = l1.next;
             }
-            return result;  
-        }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            test.next = new ListNode(sum % 10);
+            test = test.next;
+            sobe1 = sum / 10;
+            sum = 0;
 
+        }
+        if (sobe1 > 0)
+        {
+            test.next = new ListNode(sobe1);
+        }
+        return result.next;
     }
 }
